0291cb4f5f7e6e90deedc344e13d76f9127ddcf3
Ranka Jo <ranka.j.94@gmail.com>
2020-12-11 19:00:47 +0100

    test

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 
 
-    return view('welcome', ['name' => 'Ranka', 'age' => '26 ']);
+    return view('welcome', ['name' => 'Ranka', 'age' => '26']);
 });
 Route::get('/about', function () {
     return view('about');
